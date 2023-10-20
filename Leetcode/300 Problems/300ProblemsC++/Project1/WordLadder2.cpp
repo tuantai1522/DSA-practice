a5d336d054b8b7f0ce8b925d415f5d6bce30bd13
tai <2054054054tai@ou.edu.vn>
2023-10-20 22:50:05 +0700

    Add new file

---
+++ Leetcode/300 Problems/300ProblemsC++/Project1/WordLadder2.cpp
@@ -0,0 +1,55 @@
+//User function Template for C++
+
+class Solution {
+public:
+    vector<vector<string>> findSequences(string beginWord, string endWord, vector<string>& wordList) {
+    set<string> mySet(wordList.begin(), wordList.end());
+  vector<vector<string>>ans;
+
+  queue<vector<string>>my_queue;
+  my_queue.push({beginWord});
+
+  vector<string> usedOnLevels;
+  usedOnLevels.push_back({beginWord});
+
+  int level = 0;
+   while(!my_queue.empty()){
+      vector<string>top = my_queue.front();
+      my_queue.pop();
+
+      //remove all words in previous level
+      if(top.size() > level){
+        ++level;
+        for(auto it : usedOnLevels){
+          mySet.erase(it);
+        }
+        usedOnLevels.clear();
+      }
+      string last = top.back();
+
+      if(last == endWord){
+        if(ans.size() == 0 || ans[0].size() == top.size()){
+          ans.push_back(top);
+        }
+      }
+
+      
+      for(int i = 0; i < last.length(); ++i){
+        char original = last[i];
+        for(char c = 'z'; c >= 'a'; --c){
+          last[i] = c;
+          if(mySet.find(last) != mySet.end()){ // find that word in set
+            vector<string>temp = top;
+            temp.push_back(last);
+            my_queue.push(temp);
+            usedOnLevels.push_back(word); // find all words to delete in current level
+
+          }
+        }
+        last[i] = original; // to return back to original word
+      }
+
+    }
+  return ans;
+    }
+};
